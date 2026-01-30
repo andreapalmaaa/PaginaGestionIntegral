d9af248cf3099423ba9868e70e71d277620b9d31
andreapalmaaa\andre <andrea.palmaaa06@gmail.com>
2026-01-30 16:49:46 -0600

    se agrego un comentario al controlador welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,6 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		// esto es un comentari de prueba
 	}
 	public function mensaje()
 	{
